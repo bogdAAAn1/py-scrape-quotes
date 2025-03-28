c8e0040886f73090788d7155428e4462aef2695b
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-03-28 10:21:53 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,8 @@
+import csv
+import dataclasses
 from dataclasses import dataclass
-
+import requests
+from bs4 import BeautifulSoup
 
 @dataclass
 class Quote:
@@ -8,8 +11,32 @@ class Quote:
     tags: list[str]
 
 
+BASE_URL = "https://quotes.toscrape.com/"
+QUOTE_FIELDS = [f.name for f in dataclasses.fields(Quote)]
+
+
+def parse_quotes() -> list[Quote]:
+    quotes = []
+    for page in range(1, 11):
+        soup = BeautifulSoup(requests.get(f"{BASE_URL}page/{page}/").content, "html.parser")
+        for q in soup.select("div.quote"):
+            quotes.append(Quote(
+                text=q.select_one("span.text").text,
+                author=q.select_one("small.author").text,
+                tags=[tag.text for tag in q.select("div.tags a")]
+            ))
+    return quotes
+
+
+def write_quotes(quotes: list[Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([dataclasses.astuple(q) for q in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    write_quotes(parse_quotes(), output_csv_path)
 
 
 if __name__ == "__main__":
